c3d189e434849ff0b5e555fd01c1f9eca0696ed0
Felipe <felipe.teixeira@gsw.com.br>
2019-08-04 09:12:09 -0300

    init

---
+++ index.php
@@ -0,0 +1,281 @@
+<?php 
+	include 'templates/nav-bar.php';
+?>
+	
+	<header class="header">
+		<!-- Carrossel -->
+		<img src="https://picsum.photos/id/3/300/500" alt="" class="header-picture">
+	</header>
+
+	<main>
+		<!-- Destaques -->
+		<section class="content">
+			<h2 class="content-title">Destaques</h2>
+
+			<div class="cards">
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+			</div>
+
+			<div class="is-center">
+				<button type="button" class="btn btn-primary btn-small">Ver Mais</button>
+			</div>
+
+		</section>
+
+		<!-- Críticas -->
+		<section class="content full articles">
+
+			<article class="article bg-grey">
+				<img src="https://picsum.photos/id/3/300/200" alt="" class="article-picture">
+				<div class="article-body">
+					<h4 class="article-body__title">Crítica</h4>
+					<h5 class="article-body__subtitle">Los Silencios, de Beatriz Seigner</h5>
+					<p class="article-body__text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint possimus assumenda, optio
+						architecto aliquam, sed,
+						accusantium cupiditate fugit doloribus recusandae aut sequi aliquid veritatis unde iste ab dolor
+						officia.
+					</p>
+					<p class="article-body__author">
+						Por Autor Conhecido
+					</p>
+				</div>
+			</article>
+
+			<article class="article bg-blue">
+				<img src="https://picsum.photos/id/5/300/200" alt="" class="article-picture">
+				<div class="article-body">
+					<h4 class="article-body__title">Crítica</h4>
+					<h5 class="article-body__subtitle">Los Silencios, de Beatriz Seigner</h5>
+					<p class="article-body__text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint possimus assumenda, optio
+						architecto
+						aliquam, sed,
+						accusantium cupiditate fugit doloribus recusandae aut sequi aliquid veritatis unde iste ab dolor
+						officia.
+					</p>
+					<p class="article-body__author">
+						Por Autor Conhecido
+					</p>
+				</div>
+			</article>
+
+		</section>
+
+		<!-- Notícias -->
+		<section class="content">
+			<h2 class="content-title">Notícias</h2>
+
+			<section class="news">
+				<div class="news-left">
+					<article class="new">
+						<img src="https://i.imgur.com/3wTAEWW.png" alt="" class="new-picture">
+						<div class="new-content">
+							<span class="new-tag">8º Olhar de Cinema</span>
+							<h4 class="new-title">
+								Zumbilândia 2: diretor revela mais detalhes sobre a sequência
+							</h4>
+							<span class="new-datetime">
+								15/07/2019 | 14h30
+							</span>
+						</div>
+					</article>
+
+					<article class="new">
+						<img src="https://i.imgur.com/3wTAEWW.png" alt="" class="new-picture">
+						<div class="new-content">
+							<span class="new-tag">8º Olhar de Cinema</span>
+							<h4 class="new-title">
+								Zumbilândia 2: diretor revela mais detalhes sobre a sequência
+							</h4>
+							<span class="new-datetime">
+								15/07/2019 | 14h30
+							</span>
+						</div>
+					</article>
+				</div>
+
+				<div class="news-right">
+					<article class="new">
+						<img src="https://i.imgur.com/3wTAEWW.png" alt="" class="new-picture--small">
+						<div class="new-content--small">
+							<span class="new-tag">8º Olhar de Cinema</span>
+							<h4 class="new-title">
+								Zumbilândia 2: diretor revela mais detalhes sobre a sequência
+							</h4>
+							<span class="new-datetime">
+								15/07/2019 | 14h30
+							</span>
+						</div>
+					</article>
+
+					<article class="new">
+						<img src="https://i.imgur.com/3wTAEWW.png" alt="" class="new-picture--small">
+						<div class="new-content--small">
+							<span class="new-tag">8º Olhar de Cinema</span>
+							<h4 class="new-title">
+								Zumbilândia 2: diretor revela mais detalhes sobre a sequência
+							</h4>
+							<span class="new-datetime">
+								15/07/2019 | 14h30
+							</span>
+						</div>
+					</article>
+					<article class="new">
+						<img src="https://i.imgur.com/3wTAEWW.png" alt="" class="new-picture--small">
+						<div class="new-content--small">
+							<span class="new-tag">8º Olhar de Cinema</span>
+							<h4 class="new-title">
+								Zumbilândia 2: diretor revela mais detalhes sobre a sequência
+							</h4>
+							<span class="new-datetime">
+								15/07/2019 | 14h30
+							</span>
+						</div>
+					</article>
+				</div>
+
+			</section>
+		</section>
+
+		<!-- Newsletter -->
+		<section class="content newsletter">
+			<h3 class="newsletter-title">Newsletter</h3>
+			<p class="newsletter-text">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Eaque, ducimus reiciendis illo quo dolorum
+			</p>
+			<div class="is-center">
+				<button type="button" class="btn btn-default btn-small">Assinar</button>
+			</div>
+		</section>
+
+		<!-- Cobertura -->
+		<section class="content bg-lightgrey">
+			<h3 class="content-title">Coberturas</h3>
+
+			<div class="coberturas">
+
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+				
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+
+				<div class="cobertura">
+					<img src="https://picsum.photos/id/66/100/150" alt="" class="cobertura-picture">
+					<p class="cobertura-legend">
+						51º Festival de Brasília
+					</p>
+				</div>
+
+			</div>
+			<div class="is-center">
+				<button type="button" class="btn btn-primary btn-small">Ver todas</button>
+			</div>
+		</section>
+
+		<!-- Especiais -->
+		<section class="content">
+			<h2 class="content-title">Especiais</h2>
+
+			<div class="cards">
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+
+				<div class="card">
+					<img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
+					<div class="card-label">
+						<span class="card-label__text">Entrevistas</span>
+					</div>
+					<p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
+				</div>
+			</div>
+
+			<div class="is-center">
+				<button type="button" class="btn btn-primary btn-small">Ver Mais</button>
+			</div>
+
+		</section>
+
+	</main>
+	
+<?php 
+	include 'templates/footer.php';
+?>
---
+++ templates/nav-bar.php
@@ -0,0 +1,27 @@
+<?php 
+	include 'head.php';
+	include 'svg.php';
+?>
+<nav class="navbar">
+	<div class="navbar-side">
+		<svg class="icon icon-menu">
+			<use xlink:href="#icon-menu"></use>
+		</svg>
+		<div class="navbar-side--links">
+			<a href="">Entrevistas</a>
+			<a href="">Críticas</a>
+			<a href="">Coberturas a</a>
+		</div>
+	</div>
+	<img src="assets/img/logo-full.png" alt="Logo Cine Festivais" id="js-navbar-logo" class="navbar-logo">
+	<div class="navbar-side">
+		<div class="navbar-side--links">
+			<a href="">Notícias</a>
+			<a href="">Reportagens</a>
+			<a href="">Podcasts</a>
+		</div>
+		<svg class="icon icon-search1">
+			<use xlink:href="#icon-search1"></use>
+		</svg>
+	</div>
+</nav>
