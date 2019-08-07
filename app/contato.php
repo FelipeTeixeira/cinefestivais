f3628e50c7b0f2d20d97490fdc599b78d2e84069
Victor Jordan <sistema.sgo.ccb@gmail.com>
2019-08-07 12:15:41 -0300

    adding contact page

--- app/contato.php
+++ app/contato.php
@@ -2,7 +2,7 @@
 	include 'templates/nav-bar.php';
 ?>
 
-<header class="post-header full bg-img bg-quemsomos">
+<header class="post-header full bg-img bg-contato">
 </header>
 
 <article class="">
@@ -39,51 +39,15 @@
         </form>
     </section>
 
-    <?php 
+</article>
+
+<?php 
         include 'templates/newsletter.php';
     ?>
 
-    <section class="content">
-        <h2 class="content-title">Siga no cine festivais</h2>
-
-        <div class="cards">
-            <div class="card">
-                <img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
-                <div class="card-label">
-                    <span class="card-label__text">Entrevistas</span>
-                </div>
-                <p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
-            </div>
-            <div class="card">
-                <img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
-                <div class="card-label">
-                    <span class="card-label__text">Entrevistas</span>
-                </div>
-                <p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
-            </div>
-            <div class="card">
-                <img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
-                <div class="card-label">
-                    <span class="card-label__text">Entrevistas</span>
-                </div>
-                <p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
-            </div>
-
-            <div class="card">
-                <img src="https://picsum.photos/id/3/300/200" alt="" class="card-picture">
-                <div class="card-label">
-                    <span class="card-label__text">Entrevistas</span>
-                </div>
-                <p class="card-text">"Na Paraíba Anoitece": Uma conversa com Ramon Porto Mota</p>
-            </div>
-        </div>
-
-        <div class="is-center">
-            <button type="button" class="btn btn-primary btn-small">Ver Mais</button>
-        </div>
-
-    </section>
-</article>
+<?php 
+        include 'templates/sigacine.php';
+    ?>
 
 <?php 
 	include 'templates/footer.php';
